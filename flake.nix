6a8bf45d8ec857a50e4256729ac0219c7605f6cf
Joonas Rautiola <joonas.rautiola@unikie.com>
2025-11-17 20:05:45 +0200

    Improve developer experience
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- flake.nix
+++ flake.nix
@@ -3,50 +3,56 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-  let
-    systems = [ "x86_64-linux" "aarch64-linux" ];
-    forAllSystems = f:
-      nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
-  in {
-    devShells = forAllSystems (pkgs: 
+  outputs =
+    { self, nixpkgs }:
     let
-      pythonEnv = pkgs.python312.withPackages (ps: [
-        ps.pykcs11
-      ]);
-    in {
-      default = pkgs.mkShell {
-        name = "pkcs11-proxy-dev";
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (
+        pkgs:
+        let
+          pythonEnv = pkgs.python312.withPackages (ps: [
+            ps.pykcs11
+          ]);
+        in
+        {
+          default = pkgs.mkShell {
+            name = "pkcs11-proxy-dev";
 
-        nativeBuildInputs = [
-          pkgs.cmake
-          pkgs.pkg-config
-        ];
+            nativeBuildInputs = [
+              pkgs.cmake
+              pkgs.pkg-config
+            ];
 
-        buildInputs = [
-          pkgs.openssl
-          pkgs.libseccomp
-        ];
+            buildInputs = [
+              pkgs.openssl
+              pkgs.libseccomp
+            ];
 
-        packages = [
-          pythonEnv
-          pkgs.softhsm
-          pkgs.opensc
-        ];
+            packages = [
+              pythonEnv
+              pkgs.softhsm
+              pkgs.opensc
+            ];
 
-        shellHook = ''
-          echo "pkcs11-proxy dev shell with PyKCS11"
-          echo "Python with PyKCS11: $(python --version)"
-          echo "Try: python -c 'import PyKCS11; print(PyKCS11.__version__)'"
-          echo
-          echo " Building ... "
-          echo "  mkdir -p build"
-          echo "  cd build"
-          echo "  cmake .. -DCMAKE_BUILD_TYPE=Release"
-          echo "  make -j$(nproc)"
-          echo
-        '';
-      };
-    });
-  };
+            shellHook = ''
+              echo "pkcs11-proxy dev shell with PyKCS11"
+              echo "Python with PyKCS11: $(python --version)"
+              echo "Try: python -c 'import PyKCS11; print(PyKCS11.__version__)'"
+              echo
+              echo "Building instructions:"
+              echo "  cmake . -B build -DCMAKE_BUILD_TYPE=Release"
+              echo "  cd build"
+              echo "  make -j$(nproc)"
+              echo
+            '';
+          };
+        }
+      );
+    };
 }
